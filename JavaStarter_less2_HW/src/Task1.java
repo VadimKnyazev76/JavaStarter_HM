cbef8dd0fbf918146324e7d2e3580fdbe9b67c1d
VadimKnyazev76 <76048950+VadimKnyazev76@users.noreply.github.com>
2020-12-15 14:33:04 +0200

    Add files via upload

---
+++ JavaStarter_less2_HW/src/Task1.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Task1 {
+    public static void main(String[] args) {
+        double pi = 3.141592653;
+        double e = 2.7182818284590452;
+
+
+        System.out.println (pi);
+        System.out.println (e);
+
+
+    }
+}
---
+++ JavaStarter_less2_HW/src/Task2.java
@@ -0,0 +1,15 @@
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        int HHHHH = 3;
+        int dfjk = 97;
+        int kld = -999999999;
+
+        System.out.println(HHHHH);
+        System.out.println(dfjk);
+        System.out.println(kld);
+
+    }
+
+}
